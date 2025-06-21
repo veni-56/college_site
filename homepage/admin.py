104044985e664464ef7dd038b7784e3f36c38022
akddr <akddr@gmail.com>
2025-06-21 04:35:33 -0700

    Add admin.py for HomeQuickLink

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import HomePageContent,SliderImage,AboutSubmenu, AboutContentBlock,AcademicSubMenu,AcademicContentBlock,Department,Department, DepartmentContent,StudentDeskMenu,StudentDeskContentBlock,NAACSubmenu,NAACContentBlock,ActivitySection, ActivityContent
+from .models import HomePageContent,SliderImage,HomeQuickLink,AboutSubmenu, AboutContentBlock,AcademicSubMenu,AcademicContentBlock,Department,Department, DepartmentContent,StudentDeskMenu,StudentDeskContentBlock,NAACSubmenu,NAACContentBlock,ActivitySection, ActivityContent
 
 admin.site.register(HomePageContent)
 admin.site.register(SliderImage)
+admin.site.register(HomeQuickLink)
+
 
 
 class AboutContentBlockInline(admin.StackedInline):
