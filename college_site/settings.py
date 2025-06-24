1756895e52de085a5212958c4d1512e01a1e8b02
akddr <akddr@gmail.com>
2025-06-23 17:17:11 -0700

    Add SQLite3 database

--- college_site/settings.py
+++ college_site/settings.py
@@ -142,6 +142,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
