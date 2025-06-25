4fb39e7416587e8e0367a499a98c4d48f2857f2a
akddr <akddr@gmail.com>
2025-06-25 15:48:28 -0700

    Remove WHITENOISE_MEDIA_PREFIX(using Cloundinary)

--- college_site/settings.py
+++ college_site/settings.py
@@ -146,14 +146,13 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'),]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
  
-WHITENOISE_MEDIA_PREFIX = 'media/' 
 
 
 
