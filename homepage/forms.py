9c8b471fc7b534731c05065102539854b1d6c660
akddr <akddr@gmail.com>
2025-06-27 05:56:01 -0700

    Add homepage/forms.py

---
+++ homepage/forms.py
@@ -0,0 +1,7 @@
+# homepage/forms.py
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class StaffLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
