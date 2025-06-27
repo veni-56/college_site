f412daecfb4b4dc510de08d4f9b62637d21e29c0
akddr <akddr@gmail.com>
2025-06-27 06:21:46 -0700

    Add navbar.html

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import HomePageContent,StaffProfile,SliderImage,HomeQuickLink,AboutSubmenu, AboutContentBlock,AcademicSubMenu,AcademicContentBlock,Department,Department, DepartmentContent,StudentDeskMenu,StudentDeskContentBlock,NAACSubmenu,NAACContentBlock,ActivitySection, ActivityContent,
+from .models import HomePageContent,StaffProfile,SliderImage,HomeQuickLink,AboutSubmenu, AboutContentBlock,AcademicSubMenu,AcademicContentBlock,Department,Department, DepartmentContent,StudentDeskMenu,StudentDeskContentBlock,NAACSubmenu,NAACContentBlock,ActivitySection, ActivityContent,StaffProfile
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
