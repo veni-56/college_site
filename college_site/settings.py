3a4580fc64b969dd425996674db1755abcf3be0d
akddr <akddr@gmail.com>
2025-06-28 02:21:28 -0700

    Add setting.py

--- college_site/settings.py
+++ college_site/settings.py
@@ -141,7 +141,8 @@ USE_TZ = True
 # Static files settings
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"   
-STATICFILES_DIRS = [ BASE_DIR / "static" ]   
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
+
 
 # WhiteNoise settings
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
